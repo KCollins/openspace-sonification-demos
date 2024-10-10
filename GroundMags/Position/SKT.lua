cb5a80a3fc64edff52d46de4f7bb5bcd9dd4a7af
KCollins <kvc2@case.edu>
2024-10-10 17:11:13 -0400

    Updated .lua files for ground stations, based on revised template.

--- GroundMags/Position/SKT.lua
+++ GroundMags/Position/SKT.lua
@@ -1,36 +1,27 @@
--- Basic
+-- Example
 -- This asset creates a rotation that places a coordinate axes on the surface of a
 -- planetary body. The rotation causes the coordinate axes to remain fixed to the surface
 -- of the globe.
 --
--- In order for this feature to work properly, the coordinate axes need to be located at
--- the same place as well, so this example also needs a `GlobeTranslation` applied.
+-- For this feature to work properly, the coordinate axes need to be located at
+-- the same place, so this example also needs a `GlobeTranslation` applied.
 
--- The example needs a `RenderableGlobe` as a parent to function
-local Globe = {
-  Identifier = "GlobeRotation_Example_Globe",
-  Renderable = {
-    Type = "RenderableGlobe"
-  },
-  GUI = {
-    Name = "GlobeRotation - Basic (Globe)",
-    Path = "/Examples"
-  }
-}
+-- This asset is dependent on the Earth
+asset.require("scene/solarsystem/planets/earth/earth")
 
 local Node = {
   Identifier = "GlobeRotation_Example",
-  Parent = "GlobeRotation_Example_Globe",
+  Parent = "Earth",
   Transform = {
     Translation = {
       Type = "GlobeTranslation",
-      Globe = "GlobeRotation_Example_Globe",
+      Globe = "Earth",
       Latitude = 65.42,
       Longitude = 307.1
     },
     Rotation = {
       Type = "GlobeRotation",
-      Globe = "GlobeRotation_Example_Globe",
+      Globe = "Earth",
       Latitude = 65.42,
       Longitude = 307.1
     }
@@ -39,17 +30,16 @@ local Node = {
     Type = "RenderableCartesianAxes"
   },
   GUI = {
-    Name = "GlobeRotation - Basic",
+    Name = "GlobeRotation - Example",
     Path = "/Examples"
   }
 }
 
 asset.onInitialize(function()
-  openspace.addSceneGraphNode(Globe)
   openspace.addSceneGraphNode(Node)
 end)
 
 asset.onDeinitialize(function()
   openspace.removeSceneGraphNode(Node)
-  openspace.removeSceneGraphNode(Globe)
 end)
+
